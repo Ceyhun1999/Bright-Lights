eed5c3691797ecb6ce0ffc3524325476666a2073
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2022-11-20 22:28:08 +0400

    final

--- javascript.js
+++ javascript.js
@@ -88,4 +88,29 @@ window.addEventListener('resize',()=> {
     contThird.style.height = maxValue + 'px';
 },true)
 
-                
+new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".btn-next",
+      prevEl: ".btn-prev",
+    }, 
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        998: {
+            slidesPerView: 3,
+        },
+    }
+  });
